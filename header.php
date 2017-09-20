963baadfe6dcd80bcce6e398671c05c81c0b4e57
Brian Mutinda <brian.maingi716@strathmore.edu>
2017-09-20 18:34:35 +0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,29 @@
+<!--This is the header file intended to be include in each page-->
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/custom.css">
+
+		  <script type="text/javascript" src="C:\Users\Brian Mutinda\Desktop\bootstrap-3.3.7-dist\js\jquery.min.js"></script>
+		  <script type="text/javascript" src='js\jquery.js'></script>
+		  <script type="text/javascript" src="js\bootstrap.min.js"></script>
+
+	 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+</head>
+<body>
+	
+		<div class="panel panel-default" style="background-color: red; margin-bottom:0px; border: 0px;
+    border-radius: unset;">
+			<div class="panel-Body">
+				Logged In User
+			</div>
+		</div>
+	
+	
+</body>
+</html>
